fee1352e8ae5baf68581fbb122f47a0e59275a56
Malissa Romero <romero.malissa@gmail.com>
2017-05-19 16:26:28 -0400

    creating new event funtionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :set_beginning_of_week
+
+  private
+  def set_beginning_of_week
+    Date.beginning_of_week = :sunday
+  end
 end
