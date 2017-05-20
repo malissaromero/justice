65b5f13206aea75e80f85a7ad54bb35e04422c29
alitopaloglu <ali_t@gwmail.gwu.edu>
2017-05-20 10:23:19 -0400

    added functionality to all routes

--- app/controllers/calendar_items_controller.rb
+++ app/controllers/calendar_items_controller.rb
@@ -9,14 +9,35 @@ class CalendarItemsController < ApplicationController
   end
 
   def create
-    @calendar_items = CalendarItem.new(permitted_params)
+    @calendar_item = CalendarItem.new(permitted_params)
+    @calendar_item.save
+    redirect_to @calendar_item
+  end
+
+  def show
+    @id = params[:id]
+    @calendar_item = CalendarItem.find(@id)
+  end
+
+  def edit
+    @calendar_item = CalendarItem.find(params[:id])
+  end
+
+  def update
+    calendar_item = CalendarItem.find(params[:id])
+    calendar_item.update(permitted_params)
+    redirect_to calendar_item_path(calendar_item)
+  end
+
+  def destroy
+    @calendar_item = CalendarItem.find(params[:id])
+    @calendar_item.destroy
+    redirect_to calendar_items_path
   end
 
   private
   def permitted_params
-    params.require(:calendar_items).permit(
-      :title, :body, :start_time, :end_time
-    )
+    params.require(:calendar_item).permit(:title, :body, :start_time, :end_time)
   end
 
 end
